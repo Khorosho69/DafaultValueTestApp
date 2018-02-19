b2711fcc082db8ef0bf951c8975a3206cda5ed68
AntonT <turchenkoav96@gmail.com>
2018-02-19 15:22:17 +0200

    Create empty project

---
+++ app/src/main/java/com/test/antont/testapp/activities/ListActivity.java
@@ -0,0 +1,15 @@
+package com.test.antont.testapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.test.antont.testapp.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
